cd1d78e20b7d8e0c3173d72dc1322d7cb7a00b82
mwerezak <mwerezak@gmail.com>
2022-02-16 07:26:02 +0000

    Minor edits

--- src/lexer/rules.rs
+++ src/lexer/rules.rs
@@ -14,13 +14,15 @@ pub enum LexerMatch {
 pub trait LexerRule {
     fn current_state(&self) -> LexerMatch;
     
+    fn reset(&mut self);
+    
     fn feed(&mut self, ch: char) -> LexerMatch;
     
     // like feed, but only modifies the LexerRule state if would match
     // return the match state if ch was passed to feed()
     fn try_match(&mut self, ch: char) -> LexerMatch;
     
-    fn reset(&mut self);
+    // produce Some(Token) if current state is CompleteMatch, otherwise None
     fn get_token(&self) -> Option<Token>;
 }
 
