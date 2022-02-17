03e0c5af39758f8adaf1f796bf18c153e7f078a0
mwerezak <mwerezak@gmail.com>
2022-02-17 00:52:16 +0000

    Add test for CommentRule

--- src/lexer/tests.rs
+++ src/lexer/tests.rs
@@ -2,6 +2,7 @@
 
 use crate::lexer::{LexerBuilder, Token, TokenOut, Span};
 use crate::lexer::errors::{LexerError, LexerErrorType};
+use crate::lexer::rules::*;
 
 
 #[test]
@@ -299,7 +300,6 @@ fn lexer_test_matches_tokens_2() {
 fn lexer_error_invalid_token() {
     let source = "foo +bar bad baz";
     
-    // kind of contrived...
     let mut lexer = LexerBuilder::new()
         .add_rule(SingleCharRule::new(Token::IntegerLiteral(0), '+'))
         .add_rule(ExactRule::new(Token::IntegerLiteral(1), "foo"))
@@ -380,5 +380,63 @@ fn lexer_error_ambiguous_match() {
         token: Token::EOF,
         location: Span { index: 3, length: 0 },
         lineno: 1,
+    }));
+}
+
+#[test]
+fn lexer_text_comments() {
+    let source = r#"foo #bar
+    
+    #
+    # baz bad foo boo
+    
+    "#;
+    
+    let mut lexer = LexerBuilder::new()
+        .add_rule(SingleCharRule::new(Token::IntegerLiteral(0), '+'))
+        .add_rule(ExactRule::new(Token::IntegerLiteral(1), "foo"))
+        .add_rule(ExactRule::new(Token::IntegerLiteral(2), "bar"))
+        .add_rule(ExactRule::new(Token::IntegerLiteral(3), "baz"))
+        .add_rule(CommentRule::new('#'))
+        .build(source.chars());
+    
+    let out = lexer.next_token().unwrap();
+    println!("out: {:?}", out);
+    assert!(matches!(out, TokenOut {
+        token: Token::IntegerLiteral(1),
+        location: Span { index: 0, length: 3 },
+        lineno: 1,
+    }));
+    
+    let out = lexer.next_token().unwrap();
+    println!("out: {:?}", out);
+    assert!(matches!(out, TokenOut {
+        token: Token::Comment,
+        location: Span { index: 4, length: 5 },
+        lineno: 1,
+    }));
+    
+    let out = lexer.next_token().unwrap();
+    println!("out: {:?}", out);
+    assert!(matches!(out, TokenOut {
+        token: Token::Comment,
+        location: Span { index: 18, length: 2 },
+        lineno: 3,
+    }));
+    
+    let out = lexer.next_token().unwrap();
+    println!("out: {:?}", out);
+    assert!(matches!(out, TokenOut {
+        token: Token::Comment,
+        location: Span { index: 24, length: 18 },
+        lineno: 4,
+    }));
+    
+    let out = lexer.next_token().unwrap();
+    println!("out: {:?}", out);
+    assert!(matches!(out, TokenOut {
+        token: Token::EOF,
+        location: Span { index: 51, length: 0 },
+        lineno: 6,
     }));
 }
