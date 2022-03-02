b1388511e59de2f659a0da4453ff322b9d3f067a
mwerezak <mwerezak@gmail.com>
2022-03-02 01:25:33 -0500

    Plan out some future opcodes

--- src/runtime/bytecode.rs
+++ src/runtime/bytecode.rs
@@ -10,6 +10,39 @@ const OP_RETURN:        u8 = 0x0;  // return from current function
 const OP_LDCONST:       u8 = 0x1;  // load a constant from the chunk's const pool
 const OP_LDCONST_16:    u8 = 0x2;  // ...using a 16-bit index
 
+const OP_NIL:           u8 = 0x8;
+const OP_EMPTY:         u8 = 0x9;
+const OP_TRUE:          u8 = 0xA;
+const OP_FALSE:         u8 = 0xB;
+
+// Operators
+
+const OP_NEG:           u8 = 0x10;
+const OP_POS:           u8 = 0x11;
+const OP_INV:           u8 = 0x12;
+const OP_NOT:           u8 = 0x13;
+
+const OP_AND:           u8 = 0x14;
+const OP_XOR:           u8 = 0x15;
+const OP_OR:            u8 = 0x16;
+
+const OP_SHL:           u8 = 0x17;
+const OP_SHR:           u8 = 0x18;
+
+const OP_ADD:           u8 = 0x19;
+const OP_MUL:           u8 = 0x1A;
+const OP_DIV:           u8 = 0x1B;
+const OP_MOD:           u8 = 0x1C;
+
+const OP_EQ:            u8 = 0x1D;
+
+const OP_LT:            u8 = 0x1E;
+const OP_LE:            u8 = 0x1F;
+const OP_GE:            u8 = 0x20;
+const OP_GT:            u8 = 0x21;
+
+
+
 
 #[repr(u8)]
 #[derive(Clone, Copy, Hash, PartialEq, Eq)]
