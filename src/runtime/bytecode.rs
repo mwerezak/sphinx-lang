1504a4d75c7fd9925c92a71a040d0946a38a87e3
mwerezak <mwerezak@gmail.com>
2022-03-02 11:57:02 -0500

    Reorganize opcodes

--- src/runtime/bytecode.rs
+++ src/runtime/bytecode.rs
@@ -6,41 +6,51 @@ use crate::runtime::variant::Variant;
 // So if we want to convert between them and integer constants easily, 
 // we need to explictly define each value as a const
 
-const OP_RETURN:        u8 = 0x0;  // return from current function
-const OP_LDCONST:       u8 = 0x1;  // load a constant from the chunk's const pool
-const OP_LDCONST_16:    u8 = 0x2;  // ...using a 16-bit index
+// 0x00         Control
 
-const OP_NIL:           u8 = 0x8;
-const OP_EMPTY:         u8 = 0x9;
-const OP_TRUE:          u8 = 0xA;
-const OP_FALSE:         u8 = 0xB;
+const OP_RETURN:        u8 = 0x00;  // return from current function
 
-// Operators
+// 0x20-30        Immediate Values
 
-const OP_NEG:           u8 = 0x10;
-const OP_POS:           u8 = 0x11;
-const OP_INV:           u8 = 0x12;
-const OP_NOT:           u8 = 0x13;
+const OP_LDCONST:       u8 = 0x20;  // load a constant from the chunk's const pool
+const OP_LDCONST_16:    u8 = 0x21;  // ...using a 16-bit index
 
-const OP_AND:           u8 = 0x14;
-const OP_XOR:           u8 = 0x15;
-const OP_OR:            u8 = 0x16;
+const OP_NIL:           u8 = 0x30;
+const OP_EMPTY:         u8 = 0x31;
+const OP_TRUE:          u8 = 0x32;
+const OP_FALSE:         u8 = 0x33;
 
-const OP_SHL:           u8 = 0x17;
-const OP_SHR:           u8 = 0x18;
+// 0x40         Unary Operations
 
-const OP_ADD:           u8 = 0x19;
-const OP_MUL:           u8 = 0x1A;
-const OP_DIV:           u8 = 0x1B;
-const OP_MOD:           u8 = 0x1C;
+const OP_NEG:           u8 = 0x40;
+const OP_POS:           u8 = 0x41;
+const OP_INV:           u8 = 0x42;
+const OP_NOT:           u8 = 0x43;
 
-const OP_EQ:            u8 = 0x1D;
+// 0x50-60      Binary Operations
 
-const OP_LT:            u8 = 0x1E;
-const OP_LE:            u8 = 0x1F;
-const OP_GE:            u8 = 0x20;
-const OP_GT:            u8 = 0x21;
+const OP_AND:           u8 = 0x50;
+const OP_XOR:           u8 = 0x51;
+const OP_OR:            u8 = 0x52;
 
+const OP_SHL:           u8 = 0x53;
+const OP_SHR:           u8 = 0x54;
+
+const OP_ADD:           u8 = 0x55;
+const OP_SUB:           u8 = 0x56;
+const OP_MUL:           u8 = 0x57;
+const OP_DIV:           u8 = 0x58;
+const OP_MOD:           u8 = 0x59;
+
+const OP_EQ:            u8 = 0x5A;
+const OP_NE:            u8 = 0x5B;
+
+const OP_LT:            u8 = 0x5C;
+const OP_LE:            u8 = 0x5D;
+const OP_GE:            u8 = 0x5E;
+const OP_GT:            u8 = 0x5F;
+
+// 0x70         Jumps
 
 
 
