4bf1400e808a61bd53e2b320aa922e361751bc17
mwerezak <mwerezak@gmail.com>
2022-03-03 16:34:32 -0500

    Render multiline errors

--- src/frontend.rs
+++ src/frontend.rs
@@ -20,6 +20,42 @@ pub fn fmt_parser_error(fmt: &mut Formatter<'_>, error: &ParserError, symbol: &R
     write!(fmt, "{}.\n\n", message)?;
     
     // Write source line
+    fmt_source_lines(fmt, symbol)
+}
+
+fn fmt_source_lines(fmt: &mut Formatter<'_>, symbol: &ResolvedSymbol) -> fmt::Result {
+    let mut start_idx = 0;
+    for (num, raw_line) in symbol.iter_whole_lines().enumerate() {
+        let end_index = start_idx + raw_line.len(); // of current line
+        
+        let margin = format!("{: >3}", num + symbol.lineno());
+        let source_line = raw_line.trim_end();
+        
+        let start_col =
+            if symbol.start() < start_idx { 0 } // started on a previous line
+            else { symbol.start() };
+        
+        let end_col =
+            if symbol.end() > end_index { source_line.len() } // ends on a next line
+            else { symbol.end() - start_idx };
+        
+        let mut marker = String::new();
+        marker.extend(iter::repeat(' ').take(margin.len()));
+        marker.push_str("     ");
+        
+        marker.extend(iter::repeat(' ').take(start_col));
+        marker.extend(iter::repeat('^').take(end_col - start_col));
+        
+        writeln!(fmt, "{}|    {}", margin, source_line)?;
+        writeln!(fmt, "{}", marker)?;
+        
+        start_idx += raw_line.len();
+    }
+    
+    Ok(())
+}
+
+fn fmt_source_line_single(fmt: &mut Formatter<'_>, symbol: &ResolvedSymbol) -> fmt::Result {
     let margin = format!("{: >3}|    ", symbol.lineno());
     let source_line = render_symbol_single_line(symbol).to_string();
     
@@ -42,12 +78,12 @@ pub fn fmt_parser_error(fmt: &mut Formatter<'_>, error: &ParserError, symbol: &R
 }
 
 
-pub fn render_symbol_lines<'a>(symbol: &'a ResolvedSymbol) -> impl fmt::Display + 'a {
+fn render_symbol_lines<'a>(symbol: &'a ResolvedSymbol) -> impl fmt::Display + 'a {
     utils::delegate_fmt(|fmt| fmt_symbol_lines(fmt, symbol))
 }
 
 // format a symbol, including the surrounding text on the same lines, and trim trailing whitespace
-pub fn fmt_symbol_lines(fmt: &mut Formatter<'_>, symbol: &ResolvedSymbol) -> fmt::Result {
+fn fmt_symbol_lines(fmt: &mut Formatter<'_>, symbol: &ResolvedSymbol) -> fmt::Result {
     for line in symbol.iter_whole_lines() {
         fmt.write_str(line.trim_end())?;
         fmt.write_str("\n")?;
@@ -57,7 +93,7 @@ pub fn fmt_symbol_lines(fmt: &mut Formatter<'_>, symbol: &ResolvedSymbol) -> fmt
 
 // if the symbol isn't multiline, this produces the same output as render_symbol_lines()
 // if it is multiline, this renders the first line of the symbol followed by "..."
-pub fn render_symbol_single_line<'a>(symbol: &'a ResolvedSymbol) -> impl fmt::Display + 'a {
+fn render_symbol_single_line<'a>(symbol: &'a ResolvedSymbol) -> impl fmt::Display + 'a {
     utils::delegate_fmt(|fmt| fmt_symbol_single_line(fmt, symbol))
 }
 
