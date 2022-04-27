ef48e3596a197967aef09d2c7d25b567709a4337
mwerezak <mwerezak@gmail.com>
2022-04-27 16:23:40 -0400

    Move MetaObject impl for bool to a new module

--- src/runtime/types.rs
+++ src/runtime/types.rs
@@ -10,6 +10,7 @@ use crate::runtime::errors::{ExecResult, ErrorKind};
 mod ops;
 mod dispatch;
 mod metatable;
+mod boolean;
 mod numeric;
 mod string;
 mod tuple;
@@ -188,69 +189,6 @@ impl MetaObject for () {
     }
 }
 
-// Booleans
-impl MetaObject for bool {
-    fn type_tag(&self) -> Type { Type::Boolean }
-    
-    fn as_bool(&self) -> ExecResult<bool> { Ok(*self) }
-    
-    fn as_bits(&self) -> Option<ExecResult<IntType>> {
-        if *self { Some(Ok(!0)) } // all 1s
-        else { Some(Ok(0)) } // all 0s
-    }
-    
-    fn op_inv(&self) -> Option<ExecResult<Variant>> { 
-        Some(Ok(Variant::from(!(*self)))) 
-    }
-    
-    fn op_and(&self, rhs: &Variant) -> Option<ExecResult<Variant>> {
-        match rhs {
-            Variant::BoolFalse => Some(Ok(Variant::from(false))),
-            Variant::BoolTrue => Some(Ok(Variant::from(*self))),
-            _ => None,
-        }
-    }
-    fn op_rand(&self, lhs: &Variant) -> Option<ExecResult<Variant>> {
-        self.op_and(lhs)
-    }
-    
-    fn op_xor(&self, rhs: &Variant) -> Option<ExecResult<Variant>> {
-        match rhs {
-            Variant::BoolFalse => Some(Ok(Variant::from(*self))),
-            Variant::BoolTrue => Some(Ok(Variant::from(!(*self)))),
-            _ => None,
-        }
-    }
-    fn op_rxor(&self, lhs: &Variant) -> Option<ExecResult<Variant>> {
-        self.op_xor(lhs)
-    }
-    
-    fn op_or(&self, rhs: &Variant) -> Option<ExecResult<Variant>> {
-        match rhs {
-            Variant::BoolFalse => Some(Ok(Variant::from(*self))),
-            Variant::BoolTrue => Some(Ok(Variant::from(true))),
-            _ => None,
-        }
-    }
-    fn op_ror(&self, lhs: &Variant) -> Option<ExecResult<Variant>> {
-        self.op_or(lhs)
-    }
-    
-    fn cmp_eq(&self, other: &Variant) -> Option<ExecResult<bool>> {
-        match other {
-            Variant::BoolFalse => Some(Ok(!(*self))),
-            Variant::BoolTrue => Some(Ok(*self)),
-            _ => None,
-        }
-    }
-    
-    fn fmt_echo(&self) -> ExecResult<StringValue> {
-        match self {
-            true => Ok(static_symbol!("true").into()),
-            false => Ok(static_symbol!("false").into()),
-        }
-    }
-}
 
 impl<F> MetaObject for Gc<F> where F: GcTrace, Gc<F>: Callable {
     fn type_tag(&self) -> Type { Type::Function }
